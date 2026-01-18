70813fd09cd7f8c4b99cb628f269a965691a207a
languard <languard@gmail.com>
2026-01-18 15:23:09 -0500

    Initial commit

---
+++ CPP Review/main.cpp
@@ -0,0 +1,16 @@
+/*
+    CPP Assignment: Divide two numbers
+    Prompt the user for two single precision floats. 
+    Make it clear to the user which is the dividend (top) and divisor (bottom)
+    Sanity check the divisor to make sure it is not zero
+    Do the division, and display the answer to the user
+*/
+
+#include <iostream>
+
+int main()
+{
+    //Your code goes here
+}
+
+
